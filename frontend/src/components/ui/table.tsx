c2cd26212bdc86f1e6e750f5873c8cf06daa6e0e
Khushal Sharma <khushal.sharma4416@gmail.com>
2026-08-25 01:02:53 +0530

    typeerror resolved

--- frontend/src/components/ui/table.tsx
+++ frontend/src/components/ui/table.tsx
@@ -1,8 +1,7 @@
 'use client'
 
 import clsx from 'clsx'
-import type React from 'react'
-import { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useState } from 'react'
 import { Link } from './link'
 
 const TableContext = createContext<{ bleed: boolean; dense: boolean; grid: boolean; striped: boolean }>({
